bafa3cbdd1614c1cb3228bbca4c70b81b2e0d8d5
guzi <jsguzi@itcom888.com>
2019-11-02 11:08:49 +0800

    common commit

--- src/com/pattern12/Pattern12.java
+++ src/com/pattern12/Pattern12.java
@@ -1,6 +1,11 @@
 package com.pattern12;
 
 public class Pattern12 {
+
+    // 代理模式 为另一个对象提供一个替身或占位符以控制对这个对象的访问
+    // 对于TableView来说，UI服务需要访问它的行数和cell，它提供了一个代理
+    // 装饰者为对象增加行为 代理控制对象的访问
+    // Java的动态代理
     public static void main(String[] args) {
 
     }
---
+++ src/com/pattern13/Pattern13.java
@@ -0,0 +1,10 @@
+package com.pattern13;
+
+public class Pattern13 {
+
+    // 模式通常被一起使用过，并被组合在同一个设计解决方案中
+    // 复合模式在一个解决方案中结合两个或多个模式，以解决一般或重复发生的问题
+    public static void main(String[] args) {
+
+    }
+}
